e76309f82b866d241ab58c0c52a102cd7e1edad7
RevelationH <RevelationHLX@163.com>
2023-03-06 18:32:18 +0800

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -17,17 +17,17 @@ def main():
     updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
 
-    """
-    updater = Updater(token=(os.environ['ACCESS_TOKEN'], config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
-    dispatcher = updater.dispatcher
-    """
+
+    # updater = Updater(token=(os.environ['ACCESS_TOKEN'], config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # dispatcher = updater.dispatcher
+
 
     global redis1
     redis1 = redis.Redis(host=(os.environ['HOST']), password=
     (os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
-    """
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
-    """
+
+    # redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+
     # You can set this logging module, so you will know when and why things do not work as expected
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
